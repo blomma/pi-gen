ba512e1002a85d3187cf504c3ca907c8046d2e4b
Mikael Hultgren <blomma@artsoftheinsane.com>
2019-10-04 18:53:14 +0200

    House

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo apt-get install coreutils quilt parted qemu-user-static debootstrap zerofree zip dosfstools bsdtar libcap2-bin grep rsync xz-utils file git curl
+
 cat << EOF > config
 IMG_NAME=one
 ENABLE_SSH=1
