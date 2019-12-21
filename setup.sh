e0c3f049485335f6470e81ec5aafe0077482ef7d
Mikael Hultgren <blomma@artsoftheinsane.com>
2019-12-21 21:08:45 +0100

    Ginger

--- setup.sh
+++ setup.sh
@@ -4,10 +4,12 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install coreutils quilt parted qemu-user-static debootstrap zerofree zip dosfstools bsdtar libcap2-bin grep rsync xz-utils file git curl
 
+NAME="one"
+
 cat << EOF > config
-IMG_NAME=one
+IMG_NAME=$NAME
 ENABLE_SSH=1
-HOSTNAME=one
+HOSTNAME=$NAME
 LOCALE_DEFAULT="en_US.UTF-8"
 TIMEZONE_DEFAULT="Europe/Stockholm"
 DEPLOY_DIR=/projects/deploy
